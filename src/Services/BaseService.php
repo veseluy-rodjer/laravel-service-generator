8dda21a7074d389f18fb1f87063ebfe177ae7ff0
Nikolay <nikolas-ja@meta.ua>
2023-08-18 19:17:35 +0300

    add methods

--- src/Services/BaseService.php
+++ src/Services/BaseService.php
@@ -27,14 +27,39 @@ class BaseService
         return $this->repository->filter($attr, $val);
     }
 
+    public function filterIn(string $attr, array $data): Builder
+    {
+        return $this->repository->filterIn($attr, $data);
+    }
+
+    public function modelWith(string $relationship, int $id): Builder
+    {
+        return $this->repository->modelWith($relationship, $id);
+    }
+
+    public function allWith(string $relationship): Builder
+    {
+        return $this->repository->allWith($relationship);
+    }
+
     public function update(array $attributes, int $id): bool
     {
         return $this->repository->update($attributes, $id);
     }
 
+    public function massUpdate(array $attributes, string $field, array $data): bool
+    {
+        return $this->repository->massUpdate($attributes, $field, $data);
+    }
+
     public function delete(int $id): bool
     {
         return $this->repository->delete($id);
+    }
+
+    public function massDelete(string $field, array $data): bool
+    {
+        return $this->repository->massDelete($field, $data);
     }
 
     public function statusFilter(int $status): Builder
@@ -45,5 +70,10 @@ class BaseService
     public function filterByCompare(string $attr, string $compare, string|int $val): Builder
     {
         return $this->repository->filterByCompare($attr, $compare, $val);
+    }
+
+    public function sort(string $attr, string $direction = 'asc'): Builder
+    {
+        return $this->repository->sort($attr, $direction);
     }
 }
